29f972687461f50f13ff341da93563075a352455
anubhav7747 <anubhavtiwari007005@gmail.com>
2023-08-12 17:11:42 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log("Starting");
+
+function calculateAge() {
+    var userInput = document.getElementById("dob").value;
+    var dob = new Date(userInput);
+
+    var monthDifference = Date.now() - dob.getTime();
+    // console.log(monthDifference);
+
+    var dat = new Date(monthDifference);
+    // console.log(date);
+
+    var year = dat.getUTCFullYear();
+    // console.log(year);
+
+    var age = Math.abs(year - 1970);
+    // console.log(age);
+
+    return document.getElementById("age").value = age;
+}
